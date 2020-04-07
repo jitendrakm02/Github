c2e5346c06026c9da2cb60bbb6adbdfb7f6f1b67
jiten <jiten@DESKTOP-0LJ1ICR>
2020-04-07 07:43:45 +0530

    First Project

---
+++ hello/src/hello/User.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("india user details");
+		int i=-128;
+  System.out.println(i);
+  
+  int a=12999;
+  int b=1999;
+  int c=a-b;
+  System.out.println(c);
+  }
+
+}
